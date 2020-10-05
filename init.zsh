41e82a994ca96da663cf4bae6b9d6777781413fd
Branden Garrett <branden.garrett@gmail.com>
2020-10-05 18:37:17 -0400

    Add 0.1.1

--- init.zsh
+++ init.zsh
@@ -4,38 +4,24 @@
 () {
 
     # bin by docker
-    alias docker-ansible-cli='docker run --rm -it -v $(pwd):/ansible -v ~/.ssh/id_rsa:/root/.ssh/id_rsa --workdir=/ansible willhallonline/ansible:2.9-alpine /bin/sh'
-    alias docker-ansible-cmd='docker run --rm -it -v $(pwd):/ansible -v ~/.ssh/id_rsa:/root/.ssh/id_rsa --workdir=/ansible willhallonline/ansible:2.9-alpine '
+    alias docker-bin-ansible-cli='docker run --rm -it -v $(pwd):/ansible -v ~/.ssh/id_rsa:/root/.ssh/id_rsa --workdir=/ansible willhallonline/ansible:2.9-alpine /bin/sh'
+    alias docker-bin-ansible-cmd='docker run --rm -it -v $(pwd):/ansible -v ~/.ssh/id_rsa:/root/.ssh/id_rsa --workdir=/ansible willhallonline/ansible:2.9-alpine '
 
     # homebrew
     alias casks="brew search"
 
-    # docker-compose
-    alias dc='docker-compose'
-    alias dce='docker-compose exec'
-    alias dcps="dc ps"
-    alias dcu="docker-compose up -d"
-
     # dotfiles
-    alias dot='vim ~/.dotfiles'
+    alias dotvim='vim ~/.dotfiles'
+    alias dotcode='code ~/.dotfiles'
 
     # git
     alias gcm="git commit -m"
     alias gdiff="git diff"
 
-    # git-flow
-    alias gflow='git flow'
-    alias gflowf='git flow feature'
-    alias gflowfp='git flow feature publish'
-    alias gflowfs='git flow feature start'
-    alias gflowr='git flow feature'
-    alias gflowrf='git flow feature publish'
-    alias gflowrs='git flow feature start'
-
     # kubernetes
     alias kc="kubectl"
 
-    # general
+    # exit
     alias :q="exit"
     alias lock="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
 
@@ -53,9 +39,7 @@
     # zim
     alias zim="zimfw"
 
-    # ssh/sshrc/scp
+    # scp
     alias scp="scp -F ~/.ssh/config"
-    alias ssh="sshrc"
-    compdef sshrc=ssh
 
 }
